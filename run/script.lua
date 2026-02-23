020ff82411be97deff536d3fcb8876d659a71c54
mmr1337 <kolosovivan39@gmail.com>
2026-02-23 10:54:05 +0300

    Add files via upload

---
+++ run/script.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mmr1337/loader.lua/refs/heads/main/record.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mmr1337/loader.lua/refs/heads/main/rebuild.lua"))()
+--save in tdx/macros/recorder_output.json
